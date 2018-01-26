4f7ec0ba60568425809e45d151b06de8953136aa
maplesdt <maples.dan@gmail.com>
2018-01-26 15:46:31 -0500

    Creation of first UI Testing page

---
+++ xamarinPlayground/UI_Test_Page_1.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace xamarinPlayground
+{
+    public partial class UI_Test_Page_1 : ContentPage
+    {
+        public UI_Test_Page_1()
+        {
+            InitializeComponent();
+        }
+    }
+}
